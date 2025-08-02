dc436ecce8c2ea2e07a617b29b8a50674706f92d
cat-mat <cathy.osterhout@gmail.com>
2025-08-02 15:25:13 -0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cat-mat.github.io-1/sw.js', { scope: '/cat-mat.github.io-1/' })})}
